366fbf66a85658704004d438be124286f58d3259
plenum <kimjeongyoon@Tigim-Mac.local>
2017-06-09 16:10:21 +0900

    add win_build directory

---
+++ opengl-env/win_build/external/CMakeFiles/GLEW_1130.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Work/autonomous-vehicles/opengl-env/external/glew-1.13.0/src/glew.c" "C:/Work/autonomous-vehicles/opengl-env/win_build/external/CMakeFiles/GLEW_1130.dir/glew-1.13.0/src/glew.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLEW_STATIC"
+  "TW_NO_DIRECT3D"
+  "TW_NO_LIB_PRAGMA"
+  "TW_STATIC"
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../external/glfw-3.1.2/include/GLFW"
+  "../external/glew-1.13.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ opengl-env/win_build/external/glfw-3.1.2/examples/CMakeFiles/boing.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Work/autonomous-vehicles/opengl-env/external/glfw-3.1.2/examples/boing.c" "C:/Work/autonomous-vehicles/opengl-env/win_build/external/glfw-3.1.2/examples/CMakeFiles/boing.dir/boing.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLEW_STATIC"
+  "TW_NO_DIRECT3D"
+  "TW_NO_LIB_PRAGMA"
+  "TW_STATIC"
+  "UNICODE"
+  "WINVER=0x0501"
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../external/glfw-3.1.2/include"
+  "../external/glfw-3.1.2/deps"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Work/autonomous-vehicles/opengl-env/win_build/external/glfw-3.1.2/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
